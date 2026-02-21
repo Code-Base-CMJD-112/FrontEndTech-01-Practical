e0845337502429241347ea3517a68a543d7f33c7
ThanuraSilva <thanurasilva1989@gmail.com>
2026-02-21 12:52:30 +0530

    Add validations

--- app.js
+++ app.js
@@ -12,19 +12,26 @@ form.addEventListener("submit",(e)=>{
    let course = formData.get("course")
    let email = formData.get("email")
 
+   if(!firstName || !lastName || !batch || !course || !email){
+     document.getElementById("alert-pan-failed")
+     .innerHTML =" <p>Save Failed!!</p>";
+     document.getElementById("alert-pan-success").innerHTML = "";
+     return;
+   }
+
    courseData = {
-       formFirstName: firstName,
-       formLastName: lastName,
-       formBatch: batch,
-       formCourse: course,
-       formEmail: email
+       formFirstName: firstName.trim(),
+       formLastName: lastName.trim(),
+       formBatch: batch.trim(),
+       formCourse: course.trim(),
+       formEmail: email.trim()
    }
    console.log(courseData);
    console.log(JSON.stringify(courseData));
 
    
-   formData !== null ? document.getElementById("alert-pan-success").innerHTML = "<p>Saved!!</p>"
-                :document.getElementById("alert-pan-failed").innerHTML =" <p>Save Failed!!</p>"
+   document.getElementById("alert-pan-success").innerHTML = "<p>Saved!!</p>"
+  document.getElementById("alert-pan-failed").innerHTML =""
 
 })
 

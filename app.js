ad96acfa931eacfdc88a9d7c1accb9bbdd3845e2
ThanuraSilva <thanurasilva1989@gmail.com>
2026-02-21 12:12:58 +0530

    Collect form data

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log("Hello")
+const form = document.getElementById("data-from");
+let courseData = {}
+
+form.addEventListener("submit",(e)=>{
+   e.preventDefault() 
+   //collect form data
+   let formData= new FormData(form);
+
+   let firstName = formData.get("first-name")
+   let lastName = formData.get("last-name")
+   let batch = formData.get("batch")
+   let course = formData.get("course")
+   let email = formData.get("email")
+
+   courseData = {
+       formFirstName: firstName,
+       formLastName: lastName,
+       formBatch: batch,
+       formCourse: course,
+       formEmail: email
+   }
+   console.log(courseData)
+   console.log(JSON.stringify(courseData))
+
+
+
+  
+})
+
